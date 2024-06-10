5d0ccbb419afd38f881b430df2801df1e4fad3a4
ccpWebDev <159119881+ccpWebDev@users.noreply.github.com>
2024-06-10 11:58:54 +0530

    AI Basic voting system

---
+++ src/Core_Java/Basics/Operator2.java
@@ -0,0 +1,10 @@
+package Core_Java.Basics;
+// Ternary Operator
+public class Operator2 {
+    public static void main(String[] args) {
+        int a=3;
+        int i=0;
+        int result = a%2==0?1:0;
+        System.out.println(result);
+    }
+}
