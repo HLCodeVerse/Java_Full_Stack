c9851a77fe8c1be37e67ea1fc5aefbc76f2d087f
ccpWebDev <159119881+ccpWebDev@users.noreply.github.com>
2024-06-10 09:19:16 +0530

    1st Conditional Operator

---
+++ src/Core_Java/Basics/Operators1.java
@@ -0,0 +1,9 @@
+package Core_Java.Basics;
+//Conditional operator
+public class Operators1 {
+    public static void main(String[] args) {
+        int a=3,b=5,c=6,d=3;
+        boolean result= a==d && c>b;
+        System.out.println(result);
+    }
+}
