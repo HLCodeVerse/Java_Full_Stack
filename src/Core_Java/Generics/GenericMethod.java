0fac353e68652db3a398697769924d80a047016a
HLCodeVerse <hless4326@gmail.com>
2024-08-25 10:33:07 +0530

    Practice Generics: Implement Generic Method for Type-Safe Operations
    - Introduced a generic method `show<T>(T t)` that can handle and print objects of any type.
    - Demonstrated the use of the generic method with both `String` and `Integer` types.
    - Highlighted the benefits of type inference and reusability in method definitions.
    
    This commit is part of generics practice, focusing on mastering the creation of flexible and type-safe methods in Java.

--- src/Core_Java/Generics/GenericMethod.java
+++ src/Core_Java/Generics/GenericMethod.java
@@ -1,11 +1,17 @@
 package Core_Java.Generics;
 
 public class GenericMethod {
-    public static void main ( String[] args ) {
-        show( "Hello");
-        show ( 123 );
+    public static void main(String[] args) {
+        // Calling the generic method 'show' with a String argument.
+        show("Hello");
+
+        // Calling the generic method 'show' with an Integer argument.
+        show(123);
     }
-    public static <T> void show(T t){
-        System.out.println (t );
+
+    // Defining a generic method 'show' that can accept any type of object.
+    public static <T> void show(T t) {
+        // Printing the value of the object passed to the method.
+        System.out.println(t);
     }
 }
