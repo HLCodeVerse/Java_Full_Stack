a0d2024109ede0e7c00d76617adf98b9ffb4636b
HLCodeVerse <hless4326@gmail.com>
2024-08-25 10:38:22 +0530

    commit e4f2b3c7d5a8e4f2b3c7d5a8e4f2b3c7d5a8e4f2b3c7d5 Author: Chandan Panda <chandanpanda726@gmail.com> Date:   Sun Aug 25 10:37 AM 2024
        Added non-static generic method 'display' to demonstrate instance method generics
    
        - Created an instance of the GenericMethod class to call the 'display' method.
        - Demonstrated calling 'display' with both String and Integer arguments.
        - Retained the static generic method 'show' to show the contrast between static and non-static generic methods.
        - This code now effectively demonstrates the usage of both static and non-static generic methods, enhancing flexibility and understanding of generics in Java.

--- src/Core_Java/Generics/GenericMethod.java
+++ src/Core_Java/Generics/GenericMethod.java
@@ -7,11 +7,26 @@ public class GenericMethod {
 
         // Calling the generic method 'show' with an Integer argument.
         show(123);
+
+        // Creating an instance of GenericMethod to call the non-static generic method 'display'.
+        GenericMethod generic = new GenericMethod();
+
+        // Calling the non-static generic method 'display' with a String argument.
+        generic.display("World");
+
+        // Calling the non-static generic method 'display' with an Integer argument.
+        generic.display(456);
     }
 
-    // Defining a generic method 'show' that can accept any type of object.
+    // Defining a static generic method 'show' that can accept any type of object.
     public static <T> void show(T t) {
         // Printing the value of the object passed to the method.
         System.out.println(t);
+    }
+
+    // Defining a non-static generic method 'display' that can accept any type of object.
+    <E> void display(E e) {
+        // Printing the value of the object passed to the method.
+        System.out.println(e);
     }
 }
