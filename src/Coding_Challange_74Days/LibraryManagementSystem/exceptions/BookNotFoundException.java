e77826da1eccb89df46aad276b171d0959f09d1a
HLCodeVerse <hless4326@gmail.com>
2024-08-25 19:30:28 +0530

    Added Final Model and Exception Files

--- src/Coding_Challange_74Days/LibraryManagementSystem/exceptions/BookNotFoundException.java
+++
@@ -1,8 +0,0 @@
-//Day 6
-package Coding_Challange_74Days.LibraryManagementSystem.exceptions;
-
-public class BookNotFoundException extends Exception{
-    public BookNotFoundException(String message){
-        super(message);
-    }
-}
--- src/Coding_Challange_74Days/LibraryManagementSystem/exceptions/MemberNotFoundException.java
+++
@@ -1,9 +0,0 @@
-//Day 6
-package Coding_Challange_74Days.LibraryManagementSystem.exceptions;
-
-public class MemberNotFoundException extends Exception {
-    public MemberNotFoundException(String message){
-        super(message);
-    }
-
-}
