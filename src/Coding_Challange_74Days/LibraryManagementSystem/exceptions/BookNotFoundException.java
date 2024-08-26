7f9868017969f2e714548a80241e0b41435afb98
HLCodeVerse <hless4326@gmail.com>
2024-08-26 17:36:17 +0530

    added custome exceptions

---
+++ src/Coding_Challange_74Days/LibraryManagementSystem/exceptions/BookNotFoundException.java
@@ -0,0 +1,7 @@
+package Coding_Challange_74Days.LibraryManagementSystem.exceptions;
+
+public class BookNotFoundException extends Exception{
+    public BookNotFoundException(String message){
+        super(message);
+    }
+}
---
+++ src/Coding_Challange_74Days/LibraryManagementSystem/exceptions/MemberNotFoundException.java
@@ -0,0 +1,8 @@
+package Coding_Challange_74Days.LibraryManagementSystem.exceptions;
+
+public class MemberNotFoundException extends Exception {
+    public MemberNotFoundException(String message){
+        super(message);
+    }
+
+}
