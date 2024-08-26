f2b816bb4ccfc5e633ba6c38d58fe5b96be691cd
HLCodeVerse <hless4326@gmail.com>
2024-08-26 19:19:20 +0530

    made changes and implemented required things according day 11

--- src/Coding_Challange_74Days/LibraryManagementSystem/models/Book.java
+++ src/Coding_Challange_74Days/LibraryManagementSystem/models/Book.java
@@ -1,6 +1,6 @@
-//Day 1
 package Coding_Challange_74Days.LibraryManagementSystem.models;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,65 +11,87 @@ public class Book {
     private String genre;
     private boolean isAvailable;
     private List<String> reservationList;
-    public Book(String id, String title, String author, String genre, boolean isAvailable, List<String> reservationList){
+    private LocalDate borrowDate; // day 11
+    private final int borrowingPeriod = 15; // days 11
+
+    public Book(String id, String title, String author, String genre, boolean isAvailable, List<String> reservationList) {
         this.id = id;
         this.title = title;
         this.author = author;
         this.genre = genre;
         this.isAvailable = isAvailable;
-        this.reservationList = new ArrayList<> (  );
+        this.reservationList = new ArrayList<>();
+        this.borrowDate = null;
     }
-//Getter and setter Methods
-    public String getId ( ) {
+
+    // Getter and Setter Methods
+    public String getId() {
         return id;
     }
 
-    public void setId ( String id ) {
+    public void setId(String id) {
         this.id = id;
     }
 
-    public String getTitle ( ) {
+    public String getTitle() {
         return title;
     }
 
-    public void setTitle ( String title ) {
+    public void setTitle(String title) {
         this.title = title;
     }
 
-    public String getAuthor ( ) {
+    public String getAuthor() {
         return author;
     }
 
-    public void setAuthor ( String author ) {
+    public void setAuthor(String author) {
         this.author = author;
     }
 
-    public String getGenre ( ) {
+    public String getGenre() {
         return genre;
     }
 
-    public void setGenre ( String genre ) {
+    public void setGenre(String genre) {
         this.genre = genre;
     }
 
-    public boolean isAvailable ( ) {
+    public boolean isAvailable() {
         return isAvailable;
     }
 
-    public void setAvailable ( boolean available ) {
+    public void setAvailable(boolean available) {
         isAvailable = available;
     }
 
-    public List<String> getReservationList ( ) {
+    public List<String> getReservationList() {
         return reservationList;
     }
 
-    public void setReservationList ( List<String> reservationList ) {
+    public void setReservationList(List<String> reservationList) {
         this.reservationList = reservationList;
     }
-    //toString method
+
+    public LocalDate getBorrowDate() {
+        return borrowDate;
+    }
+
+    public void setBorrowDate(LocalDate borrowDate) {
+        this.borrowDate = borrowDate;
+    }
+
+    public int getBorrowingPeriod() {
+        return borrowingPeriod;
+    }
+
+    // Method to get the current date
+    public static LocalDate getCurrentDate() {
+        return LocalDate.now();
+    }
+
     @Override
-    public String toString ( ) {
+    public String toString() {
         return "Book{" +
                 "id='" + id + '\'' +
                 ", title='" + title + '\'' +
@@ -77,6 +99,7 @@ public class Book {
                 ", genre='" + genre + '\'' +
                 ", isAvailable=" + isAvailable +
                 ", reservationList=" + reservationList +
+                ", borrowDate=" + borrowDate +
                 '}';
     }
 }
